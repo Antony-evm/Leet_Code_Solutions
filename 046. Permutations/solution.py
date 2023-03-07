32ae837e4f2d8893ae69f7989d4e664f66264f6f
Antony-evm <60288950+Antony-evm@users.noreply.github.com>
2023-03-07 16:56:30 +0000

    Create solution.py

---
+++ 046. Permutations/solution.py
@@ -0,0 +1,7 @@
+# Given an array nums of distinct integers, return all the possible permutations. You can return the answer in any order.
+
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        import itertools
+
+        return list(itertools.permutations(nums))
