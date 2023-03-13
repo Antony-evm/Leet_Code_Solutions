b3679c18e2d2ac28d28f19c389a97a7e14424043
Antony-evm <60288950+Antony-evm@users.noreply.github.com>
2023-03-13 10:49:54 +0000

    Create solution.py

---
+++ 704. Binary Search/solution.py
@@ -0,0 +1,9 @@
+# Given an array of integers nums which is sorted in ascending order, and an integer target, write a function to search target in nums. If target exists, then return its index. Otherwise, return -1.
+
+# You must write an algorithm with O(log n) runtime complexity.
+
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        left = bisect.bisect_left(nums,target)
+        right = bisect.bisect_left(nums,target+1)
+        return left if left!=right else -1
