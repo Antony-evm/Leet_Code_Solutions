67ce82adea43890d2a32808fe6ff2d586ee24444
Antony-evm <60288950+Antony-evm@users.noreply.github.com>
2023-03-17 07:00:12 +0000

    Create solution.py

---
+++ 589. N-ary Tree Preorder Traversal/solution.py
@@ -0,0 +1,25 @@
+# Given the root of an n-ary tree, return the preorder traversal of its nodes' values.
+
+# Nary-Tree input serialization is represented in their level order traversal. Each group of children is separated by the null value (See examples)
+
+ 
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val=None, children=None):
+        self.val = val
+        self.children = children
+"""
+
+class Solution:
+    def preorder(self, root: 'Node') -> List[int]:
+        if root is None:
+            return []
+        
+        stack = [root]
+        output = []
+        while stack:
+            temp = stack.pop()
+            output.append(temp.val)
+            stack.extend(temp.children[::-1])
+        return output
