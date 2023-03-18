b98dcf169f7176cefc01f3c8ec3cb16ed82b0e77
Antony-evm <60288950+Antony-evm@users.noreply.github.com>
2023-03-18 06:08:49 +0000

    Create solution.py

---
+++ 383. Ransom Note/solution.py
@@ -0,0 +1,11 @@
+# Given two strings ransomNote and magazine, return true if ransomNote can be constructed by using the letters from magazine and false otherwise.
+
+# Each letter in magazine can only be used once in ransomNote.
+
+class Solution:
+    def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+        ransomLetters = list(dict.fromkeys(ransomNote))
+        for letter in ransomLetters:
+            if ransomNote.count(letter)>magazine.count(letter):
+                return False
+        return True
