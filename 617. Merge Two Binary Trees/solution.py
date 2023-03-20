f345c641cef367b7880daf9e62997ccba4395ece
Antony-evm <60288950+Antony-evm@users.noreply.github.com>
2023-03-20 09:00:34 +0000

    Create solution.py

---
+++ 617. Merge Two Binary Trees/solution.py
@@ -0,0 +1,35 @@
+# You are given two binary trees root1 and root2.
+
+# Imagine that when you put one of them to cover the other, some nodes of the two trees are overlapped while the others are not. You need to merge the two trees into a new binary tree. The merge rule is that if two nodes overlap, then sum node values up as the new value of the merged node. Otherwise, the NOT null node will be used as the node of the new tree.
+
+# Return the merged tree.
+
+# Note: The merging process must start from the root nodes of both trees.
+
+ 
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def mergeTrees(self, root1: Optional[TreeNode], root2: Optional[TreeNode]) -> Optional[TreeNode]:
+        t1,t2 = root1,root2
+        if not (t1 and t2):
+            return t1 or t2
+        queue1, queue2 = collections.deque([t1]), collections.deque([t2])
+        while queue1 and queue2:
+            node1, node2 = queue1.popleft(), queue2.popleft()
+            if node1 and node2:
+                node1.val = node1.val + node2.val
+                if (not node1.left) and node2.left:
+                    node1.left = TreeNode(0)
+                if (not node1.right) and node2.right:
+                    node1.right = TreeNode(0)
+                queue1.append(node1.left)
+                queue1.append(node1.right)
+                queue2.append(node2.left)
+                queue2.append(node2.right)
+        return t1
+
