3c17faf003e1d4eee22a2725670b57f63ee18a08
Antony-evm <60288950+Antony-evm@users.noreply.github.com>
2023-03-22 05:38:08 +0000

    Create solution.py

---
+++ 404. Sum of Left Leaves/solution.py
@@ -0,0 +1,26 @@
+# Given the root of a binary tree, return the sum of all left leaves.
+
+# A leaf is a node with no children. A left leaf is a leaf that is the left child of another node.
+
+ 
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def sumOfLeftLeaves(self, root: Optional[TreeNode]) -> int:
+        res = 0
+        stack = [root]
+        while stack:
+            curr = stack.pop(0)
+            if curr.left:
+                stack.append(curr.left)
+                if curr.left.left==None and curr.left.right==None:
+                    res+=curr.left.val
+            if curr.right:
+                stack.append(curr.right)
+            
+        return res
