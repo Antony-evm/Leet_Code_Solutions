0a9c7a7a1ac2f14192340e7c644862dd63e6bd09
Antony-evm <60288950+Antony-evm@users.noreply.github.com>
2023-04-01 10:48:31 +0100

    solutions

---
+++ 48. Rotate Image/solution.py
@@ -0,0 +1,10 @@
+# You are given an n x n 2D matrix representing an image, rotate the image by 90 degrees (clockwise).
+
+# You have to rotate the image in-place, which means you have to modify the input 2D matrix directly. DO NOT allocate another 2D matrix and do the rotation.
+
+class Solution:
+    def rotate(self, A):
+        A.reverse()
+        for i in range(len(A)):
+            for j in range(i):
+                A[i][j], A[j][i] = A[j][i], A[i][j]
---
+++ 844. Backspace String Compare/solution.py
@@ -0,0 +1,21 @@
+# Given two strings s and t, return true if they are equal when both are typed into empty text editors. '#' means a backspace character.
+
+# Note that after backspacing an empty text, the text will continue empty.
+
+class Solution:
+    def backspaceCompare(self, s: str, t: str) -> bool:
+        def backspace(s,l,r):
+            while l<r:
+                if s[l]=='#' and l>0:
+                    s=s[:l-1]+s[l+1:]
+                    r-=2
+                    l-=2
+                elif s[l]=='#' and l==0:
+                    s=s[l+1:]
+                    r-=1
+                    l-=1
+                l+=1
+            return s
+        
+        return backspace(s,0,len(s)) == backspace(t,0,len(t))
+        
