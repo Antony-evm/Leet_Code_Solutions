3b377e68bd897195328651c939cd8fdb8750f003
Antony-evm <60288950+Antony-evm@users.noreply.github.com>
2023-04-06 05:30:47 +0100

    sols

---
+++ 078. Subsets/solution.py
@@ -0,0 +1,13 @@
+# Given an integer array nums of unique elements, return all possible 
+# subsets
+#  (the power set).
+
+# The solution set must not contain duplicate subsets. Return the solution in any order.
+
+
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        res = []
+        for i in range(len(nums)+1):
+            res+=list(itertools.combinations(nums,i))
+        return res
---
+++ 797. All Paths From Source to Target/solution.py
@@ -0,0 +1,27 @@
+# Given a directed acyclic graph (DAG) of n nodes labeled from 0 to n - 1, find all possible paths from node 0 to node n - 1 and return them in any order.
+
+# The graph is given as follows: graph[i] is a list of all nodes you can visit from node i (i.e., there is a directed edge from node i to node graph[i][j]).
+
+class Solution:
+    def allPathsSourceTarget(self, graph: List[List[int]]) -> List[List[int]]:
+        stack = deque(graph[0])
+        paths = deque()
+        graph[-1]=[]
+        res = []
+        
+        while stack:
+            paths.append([stack.popleft()])
+            ele = paths.pop()
+            t = deque([ele])
+            while t:
+                ele = t.popleft()
+                if graph[ele[-1]]:
+                    for new_node in graph[ele[-1]]:
+                        t.append(ele+[new_node])
+                else:
+                    if ele[-1]==len(graph)-1:
+                        res.append([0]+ele)
+                
+            
+            
+        return res
