7fec134da43903bc7ad0bbf747b0690b5c828cb8
Antony-evm <60288950+Antony-evm@users.noreply.github.com>
2023-04-06 17:14:29 +0100

    Update solution.py

--- 090. Subsets II/solution.py
+++ 090. Subsets II/solution.py
@@ -16,3 +16,15 @@ class Solution:
         return res
 
         
+class Solution:
+    def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
+        res = []
+        nums.sort()
+        def dfs(idx, path):
+            res.append(path)
+            for i in range(idx, len(nums)):
+                if i > idx and nums[i] == nums[i-1]:
+                    continue
+                dfs(i+1, path+[nums[i]])
+        dfs(0, [])
+        return res
