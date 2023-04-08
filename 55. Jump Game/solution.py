ff337551e25e5a04ffff6d6fd28b00939d175e21
Antony-evm <60288950+Antony-evm@users.noreply.github.com>
2023-04-08 07:48:53 +0100

    Update solution.py

--- 55. Jump Game/solution.py
+++ 55. Jump Game/solution.py
@@ -12,3 +12,19 @@ class Solution:
             if (i + nums[i]) >= last_position:
                 last_position = i
         return last_position == 0	
+
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        n = len(nums)
+        
+        @lru_cache(None)
+        def dp(i):
+            if i == n - 1:
+                return True
+            
+            for j in range(i+1, min(i+nums[i], n-1) + 1):
+                if dp(j):
+                    return True
+            return False
+        
+        return dp(0)
