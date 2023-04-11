cc4bbff39a92ea1c314c0bfe4e2326e12c083cdb
Antony-evm <60288950+Antony-evm@users.noreply.github.com>
2023-04-11 15:12:49 +0100

    Update solution.py

--- 201. Bitwise AND of Numbers Range/solution.py
+++ 201. Bitwise AND of Numbers Range/solution.py
@@ -8,3 +8,9 @@ class Solution:
             right >>= 1
             i += 1
         return right << i
+
+class Solution:
+    def rangeBitwiseAnd(self, left: int, right: int) -> int:
+        while(right>left):
+           right = right & right-1
+        return left & right
