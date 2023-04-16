4080b99d9f8b8dea7e75989aedd811be7f922529
Antony-evm <60288950+Antony-evm@users.noreply.github.com>
2023-04-16 12:22:08 +0100

    Create solution.py

---
+++ 384. Shuffle an Array/solution.py
@@ -0,0 +1,34 @@
+# Given an integer array nums, design an algorithm to randomly shuffle the array. All permutations of the array should be equally likely as a result of the shuffling.
+
+# Implement the Solution class:
+
+# Solution(int[] nums) Initializes the object with the integer array nums.
+# int[] reset() Resets the array to its original configuration and returns it.
+# int[] shuffle() Returns a random shuffling of the array.
+ 
+
+class Solution:
+
+    def __init__(self, nums: List[int]):
+      self.nums = nums
+
+    def reset(self) -> List[int]:
+      return self.nums
+        
+
+    def shuffle(self) -> List[int]:
+      keys = {idx:i for idx,i in enumerate(self.nums)}
+      res = [None]*len(self.nums)
+      for i in range(len(self.nums)):
+        rand = choice(list(keys.keys()))
+        res[i] = keys[rand]
+        keys.pop(rand)
+      return res
+      
+        
+
+
+# Your Solution object will be instantiated and called as such:
+# obj = Solution(nums)
+# param_1 = obj.reset()
+# param_2 = obj.shuffle()
