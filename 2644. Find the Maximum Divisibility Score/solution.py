1f4fb3067cadf8220625d666b8b2740d4b65c251
Antony-evm <60288950+Antony-evm@users.noreply.github.com>
2023-04-17 16:40:59 +0100

    Create solution.py

---
+++ 2644. Find the Maximum Divisibility Score/solution.py
@@ -0,0 +1,11 @@
+# You are given two 0-indexed integer arrays nums and divisors.
+
+# The divisibility score of divisors[i] is the number of indices j such that nums[j] is divisible by divisors[i].
+
+# Return the integer divisors[i] with the maximum divisibility score. If there is more than one integer with the maximum score, return the minimum of them.
+
+class Solution:
+    def maxDivScore(self, nums: List[int], divisors: List[int]) -> int:
+        res = max(sorted([(sum([j%i==0 for j in nums]),i) for i in divisors],key=lambda x:x[1]),key=lambda x:x[0])
+        return res[1]
+        
