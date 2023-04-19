c8241d0f0acccd8cd260d45b6d67e70f09e62712
Antony-evm <60288950+Antony-evm@users.noreply.github.com>
2023-04-19 13:03:22 +0100

    Update solution.py

--- 1480. Running Sum of 1d Array/solution.py
+++ 1480. Running Sum of 1d Array/solution.py
@@ -6,3 +6,12 @@
 class Solution:
     def runningSum(self, nums: List[int]) -> List[int]:
         return list(accumulate(nums))
+
+class Solution:
+    def runningSum(self, nums: List[int]) -> List[int]:
+        rs = 0
+        for i in range(len(nums)):
+            rs += nums[i]
+            nums[i] = rs
+
+        return nums
