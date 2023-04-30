0187a47451eda80f7b1586962154dd77fda73f6a
Antony-evm <60288950+Antony-evm@users.noreply.github.com>
2023-04-30 10:03:46 +0100

    solutions

---
+++ 070. Climbing Stairs/solution.py
@@ -0,0 +1,10 @@
+# You are climbing a staircase. It takes n steps to reach the top.
+
+# Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
+
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        a, b = 0, 1
+        for i in range(0, n+1):
+            a, b = b, a + b
+        return a
