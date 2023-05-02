8ad00eda8c9e595b5d67154e0500b947f4b23071
Antony-evm <60288950+Antony-evm@users.noreply.github.com>
2023-05-02 12:31:58 +0100

    Create solution.py

---
+++ 746. Min Cost Climbing Stairs/solution.py
@@ -0,0 +1,16 @@
+# You are given an integer array cost where cost[i] is the cost of ith step on a staircase. Once you pay the cost, you can either climb one or two steps.
+
+# You can either start from the step with index 0, or the step with index 1.
+
+# Return the minimum cost to reach the top of the floor.
+
+class Solution:
+    def minCostClimbingStairs(self, cost: List[int]) -> int:
+        cur=0
+        dp0,dp1=cost[0],cost[1]
+        
+        for i in range(2, len(cost)):
+            cur=cost[i] + min(dp0,dp1)
+            dp0=dp1
+            dp1=cur
+        return min(dp0,dp1)
